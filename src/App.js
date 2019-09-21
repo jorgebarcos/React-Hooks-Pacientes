f6f2988d67bb7f5a5ce27f0819a6739317580f1e
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-21 09:04:47 -0300

    Creando el COmponente de Formulario

--- src/App.js
+++ src/App.js
@@ -1,4 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+
+function Formulario() {
+	return (
+		<Fragment>
+			<h2>Crear Cita</h2>
+			<form>
+				<label>Nombre Mascota</label>
+				<input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" />
+
+				<label>Nombre Dueño</label>
+				<input
+					type="text"
+					name="propietario"
+					className="u-full-width"
+					placeholder="Nombre Dueño de la Mascota"
+				/>
+
+				<label>Fecha</label>
+				<input type="date" className="u-full-width" name="fecha" />
+
+				<label>Hora</label>
+				<input type="time" className="u-full-width" name="hora" />
+
+				<label>Sintomas</label>
+				<textarea className="u-full-width" name="sintomas" />
+
+				<button type="submit" className="button-primary u-full-width">
+					Agregar
+				</button>
+			</form>
+		</Fragment>
+	);
+}
 
 function App() {
 	// useState retorna 2 funciones
@@ -7,7 +40,19 @@ function App() {
 
 	const [ citas, guardarCita ] = useState([]);
 
-	return <h1>hola</h1>;
+	return (
+		<Fragment>
+			<h1>Administrador de Pacientes</h1>
+			<div className="container">
+				<div className="row">
+					<div className="one-half colum">
+						<Formulario />
+					</div>
+					<div className="one-half colum" />
+				</div>
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
